e225ffdb08930b2e4c59a044a785a7eaef9cab62
Daniel Peacock <daniel.peacock@citrix.com>
2022-04-11 23:17:48 +0100

    initial commit

---
+++ build-app.tf
@@ -0,0 +1,29 @@
+
+# Create the resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "acme"
+  location = "uksouth"
+}
+# Create the Linux App Service Plan
+resource "azurerm_app_service_plan" "appserviceplan" {
+  name                = "webapp-acme=1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+# Create the web app, pass in the App Service Plan ID, and deploy code from a public GitHub repo
+resource "azurerm_app_service" "webapp" {
+  name                = "webapp-acme-1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
+  source_control {
+    repo_url           = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
+    branch             = "master"
+    manual_integration = true
+    use_mercurial      = false
+  }
+}
