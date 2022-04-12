396d32872292578b64a04b81cdd96ced3439c950
Daniel Peacock <daniel.peacock@citrix.com>
2022-04-12 22:21:52 +0100

    remove unsupported args

--- build-app.tf
+++ build-app.tf
@@ -31,6 +31,7 @@ resource "azurerm_resource_group" "acme" {
   name     = "acme"
   location = "westeurope"
 }
+
 # Create the Linux App Service Plan
 resource "azurerm_app_service_plan" "sp-webapp-acme-1" {
   name                = "sp-webapp-acme-1"
@@ -41,6 +42,7 @@ resource "azurerm_app_service_plan" "sp-webapp-acme-1" {
     size = "F1"
   }
 }
+
 # Create the web app, pass in the App Service Plan ID, and deploy code from a public GitHub repo
 resource "azurerm_app_service" "sa-webapp-acme-1" {
   name                = "sa-webapp-acme-1"
@@ -50,7 +52,5 @@ resource "azurerm_app_service" "sa-webapp-acme-1" {
   source_control {
     repo_url           = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
     branch             = "master"
-    manual_integration = true
-    use_mercurial      = false
   }
 }
