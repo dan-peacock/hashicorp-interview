db36f7274feba470be97c333c6118d60db9ca6f0
Daniel Peacock <daniel.peacock@citrix.com>
2022-04-12 01:47:44 +0100

    var cleanup

--- variables.tf
+++ variables.tf
@@ -1,25 +1,4 @@
-variable "resource_group_name_prefix" {
-  default       = "rg"
-  description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
-}
-
 variable "resource_group_location" {
-  default = "westeurope"
+  default = "	westeurope"
   description   = "Location of the resource group."
-}
-
-variable "subscriptionID" {
-  type = string
-}
-
-variable "clientID" {
-  type = string
-}
-
-variable "clientSecret" {
-  type = string
-}
-
-variable "tenantID" {
-  type = string
 }
