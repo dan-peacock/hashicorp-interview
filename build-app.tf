ac8b3ebdb8fd8b46447bf38d0f19b72815e914fe
Daniel Peacock <daniel.peacock@citrix.com>
2022-04-12 23:29:02 +0100

    namespace caps

--- build-app.tf
+++ build-app.tf
@@ -9,7 +9,7 @@ provider "vault" {
   address = "https://vault-SP.vault.5b9819f8-78c7-4299-bd66-bed672713bca.aws.hashicorp.cloud:8200"
   auth_login {
     token = var.token
-    namespace = admin
+    namespace = "admin"
   }
 }
 
