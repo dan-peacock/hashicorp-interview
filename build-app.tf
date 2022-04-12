6cf02d82129a952afd0ecec7cd5d4e18a3b38b20
Daniel Peacock <daniel.peacock@citrix.com>
2022-04-12 01:25:08 +0100

    fix rg name

--- build-app.tf
+++ build-app.tf
@@ -1,11 +1,11 @@
 
 # Create the resource group
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group_test" "rg" {
   name     = "acme"
-  location = "uksouth"
+  location = "westeurope"
 }
 # Create the Linux App Service Plan
-resource "azurerm_app_service_plan" "appserviceplan" {
+resource "azurerm_app_service_plan_test" "appserviceplan" {
   name                = "webapp-acme=1"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -15,7 +15,7 @@ resource "azurerm_app_service_plan" "appserviceplan" {
   }
 }
 # Create the web app, pass in the App Service Plan ID, and deploy code from a public GitHub repo
-resource "azurerm_app_service" "webapp" {
+resource "azurerm_app_service_test" "webapp" {
   name                = "webapp-acme-1"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
