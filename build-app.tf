5797d1890486ca77fe8c3745bc735920c59c4bea
Daniel Peacock <daniel.peacock@citrix.com>
2022-04-12 22:18:37 +0100

    simplify config structure

--- build-app.tf
+++ build-app.tf
@@ -1,7 +1,29 @@
+provider "vault" {
+  address = "https://vault-cluster.vault.5b9819f8-78c7-4299-bd66-bed672713bca.aws.hashicorp.cloud:8200"
+
+  auth_login {
+    path = var.vault_username
+
+    parameters = {
+      password = var.vault_password
+    }
+  }
+}
+
 data "vault_azure_access_credentials" "creds" {
   backend        = "azure"
   role           = "ns-msdn-contributor"
   validate_creds = true
+}
+
+provider "azurerm" {
+  disable_terraform_partner_id = true
+  version                      = "=2.0"
+  tenant_id                    = var.tenant_id
+  subscription_id              = var.subscription_id
+  client_id                    = data.vault_azure_access_credentials.creds.client_id
+  client_secret                = data.vault_azure_access_credentials.creds.client_secret
+  features {}
 }
 
 # Create the resource group
