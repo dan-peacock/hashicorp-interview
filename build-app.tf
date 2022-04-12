281dd60472dc6bcbf9469253e397d974f56487c3
Daniel Peacock <daniel.peacock@citrix.com>
2022-04-12 01:34:13 +0100

    idk

--- build-app.tf
+++ build-app.tf
@@ -5,21 +5,21 @@ resource "azurerm_resource_group" "acme" {
   location = "westeurope"
 }
 # Create the Linux App Service Plan
-resource "azurerm_app_service_plan" "webapp-acme-1" {
-  name                = "webapp-acme-1"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_app_service_plan" "sp-webapp-acme-1" {
+  name                = "sp-webapp-acme-1"
+  location            = azurerm_resource_group.acme.location
+  resource_group_name = azurerm_resource_group.acme.name
   sku {
     tier = "Free"
     size = "F1"
   }
 }
 # Create the web app, pass in the App Service Plan ID, and deploy code from a public GitHub repo
-resource "azurerm_app_service" "webapp-acme-1" {
-  name                = "webapp-acme-1"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.appserviceplan.name
+resource "azurerm_app_service" "sa-webapp-acme-1" {
+  name                = "sa-webapp-acme-1"
+  location            = azurerm_resource_group.acme.location
+  resource_group_name = azurerm_resource_group.acme.name
+  app_service_plan_id = azurerm_app_service_plan.acme.id
   source_control {
     repo_url           = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
     branch             = "master"
