c38db33c61fe5076c0dca760ecc87a48bf8537fb
Daniel Peacock <daniel.peacock@citrix.com>
2022-04-13 13:21:33 +0100

    update url

--- build-app.tf
+++ build-app.tf
@@ -10,7 +10,7 @@ variable VAULT_NAMESPACE {
 }
 
 provider vault {
-  address = "https://vault-SP.vault.5b9819f8-78c7-4299-bd66-bed672713bca.aws.hashicorp.cloud:8200"
+  address = "https://vault-cluster.vault.5b9819f8-78c7-4299-bd66-bed672713bca.aws.hashicorp.cloud:8200"
   auth_login {
     path = "auth/approle/login"
     namespace = "admin" # only needed if auth method is mounted within another namespace (enterprise only)
