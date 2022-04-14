4fe0ad7d81f581ef4714307cf9d96244a59fc4a3
Daniel Peacock <daniel.peacock@citrix.com>
2022-04-14 03:54:18 +0100

    fix rg name

--- build-app.tf
+++ build-app.tf
@@ -46,9 +46,8 @@ resource "azurerm_resource_group" "acme" {
 #Create Storage account
 resource "azurerm_storage_account" "storage_account" {
   name                = var.storage_account
-  resource_group_name = azurerm_resource_group.resource_group.name
-
-  location                 = var.location
+  resource_group_name = azurerm_resource_group.acme.name
+  location                 = var.resource_group_location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   account_kind             = "StorageV2"
--- output.tf
+++
@@ -1,3 +0,0 @@
-output "app_service_id" {
-    value = azurerm_app_service_plan.sp-webapp-acme-1.id
-}
